04b1d5ac6f8b5b096bc27fcab666d453075de6bd
NadegeR <nadegerampon@yahoo.fr>
2022-05-18 17:15:25 +0200

    entity Ville Lieu Sortie et page liste sorties

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -3,8 +3,13 @@
 namespace App\Form;
 
 use App\Entity\Sortie;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,12 +18,12 @@ class SortieType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('dateHeureDebut')
-            ->add('duree')
-            ->add('dateLimiteInscription')
-            ->add('nbIscriptionsMax')
-            ->add('infosSortie')
+            ->add('nom', TextType::class, ['label'=> 'Nom'])
+            ->add('dateHeureDebut', DateTimeType::class, ['html5'=> true,'widget'=> 'single_text'] )
+            ->add('duree', TimeType::class, ['html5'=> true,'widget'=> 'single_text'])
+            ->add('dateLimiteInscription',DateTimeType::class, ['html5'=> true,'widget'=> 'single_text'])
+            ->add('nbIscriptionsMax',IntegerType::class, ['attr'=>['min'=>0], 'label'=> 'Places disponibles'])
+            ->add('infosSortie', TextareaType::class, ['label'=> 'Description de la sortie'])
             ->add('enregistrer', SubmitType::class, ['label' => 'Enregistrer', 'attr' => [
                     'class' => 'btn btn-outline-primary col-lg-1']
             ])
