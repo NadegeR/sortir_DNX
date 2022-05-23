201c47834bace26b7c533b24b9ba69a3c346e6bd
Xav ™ <xavche38@gmail.com>
2022-05-23 12:16:39 +0200

    recherche par filtres

--- src/Repository/SortieRepository.php
+++ src/Repository/SortieRepository.php
@@ -39,6 +39,54 @@ class SortieRepository extends ServiceEntityRepository
         }
     }
 
+    public function sortieParFiltres($filtre): array
+    {
+        $query = $this->createQueryBuilder('sortie');
+
+        if ($filtre['campus']) {
+            $query->andWhere('sortie.siteOrganisateur = :campus')
+                  ->setParameter('campus', $filtre['campus']);
+        }
+
+        if ($filtre['nom']) {
+            $query->andWhere('sortie.nom LIKE :nom')
+                  ->setParameter('nom', '%' . $filtre['nom'] . '%');
+        }
+
+        if ($filtre['dateDebut'] || $filtre['dateFin']) {
+            $dateDebut = $filtre['dateDebut'] ? date_format($filtre['dateDebut'], 'd-m-Y') : date('d-m-Y');
+            $dateFin = $filtre['dateFin'] ? date_format($filtre['dateFin'], 'd-m-Y') : date('d-m-Y');
+            $query->andWhere('sortie.dateHeureDebut BETWEEN :dateDebut and :dateFin')
+                  ->setParameter('dateDebut', $dateDebut)
+                  ->setParameter('dateFin', $dateFin);
+        }
+
+        if ($filtre['organisateur']) {
+            $query->andWhere('sortie.organisateurs = :organisateur')
+                  ->setParameter('organisateur', $filtre['userID']);
+        }
+
+        if($filtre['inscrit']){
+            $query ->andWhere(':userID MEMBER OF sortie.isInscrit')
+                   ->setParameter('userID', $filtre['userID']);
+        }
+
+        if($filtre['nonInscrit']) {
+            $query->andWhere(':userId NOT MEMBER OF sortie.isInscrit')
+                  ->setParameter('userID', $filtre['userID'])
+                  ->andWhere('sortie.organisateurs != :organisateur')
+                  ->setParameter('organisateur', $filtre['userID']);
+        }
+
+        if($filtre['passee']){
+
+        }
+
+
+
+}
+
+
 //    /**
 //     * @return Sortie[] Returns an array of Sortie objects
 //     */
